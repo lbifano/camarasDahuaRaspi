9927502d5e9c29fe2e8e8b35f0599227890d6bce
Lautaro Bifano <bifanolautaro@gmail.com>
2020-06-28 12:09:26 -0300

    primer commit

--- camaras2.sh
+++ camaras2.sh
@@ -2,16 +2,16 @@ python3 /home/pi/get_temp.py > /home/pi/custom.srt
 
 sleep 1
 
-omxplayer --win '0 0 512 384' -o hdmi rtsp://admin:Qwerty01@192.168.88.108:554/cam/realmonitor?channel=1%26subtype=1 &
+omxplayer --win '0 0 512 384' -o hdmi rtsp://USUARIO:CLAVE@192.168.88.108:554/cam/realmonitor?channel=1%26subtype=1 &
 
 
-omxplayer --win '512 0 1024 384' -o hdmi rtsp://admin:Qwerty01@192.168.88.108:554/cam/realmonitor?channel=6%26subtype=1 &
+omxplayer --win '512 0 1024 384' -o hdmi rtsp://USUARIO:CLAVE@192.168.88.108:554/cam/realmonitor?channel=6%26subtype=1 &
 
 
-omxplayer --win '0 384 512 768' -o hdmi rtsp://admin:Qwerty01@192.168.88.108:554/cam/realmonitor?channel=4%26subtype=1 &
+omxplayer --win '0 384 512 768' -o hdmi rtsp://USUARIO:CLAVE@192.168.88.108:554/cam/realmonitor?channel=4%26subtype=1 &
 
 
-omxplayer  --subtitles /home/pi/custom.srt --win '512 384 1024 768' -o hdmi rtsp://admin:Qwerty01@192.168.88.108:554/cam/realmonitor?channel=2%26subtype=1 &
+omxplayer  --subtitles /home/pi/custom.srt --win '512 384 1024 768' -o hdmi rtsp://USUARIO:CLAVE@192.168.88.108:554/cam/realmonitor?channel=2%26subtype=1 &
 
 sleep 600
 killall omxplayer.bin
